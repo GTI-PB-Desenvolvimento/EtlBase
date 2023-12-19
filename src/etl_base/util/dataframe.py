e7ab9406451967dccf262ebda47d8412c7a51241
Pedro Henrick <pedrohenrickqa@gmail.com>
2023-12-19 17:01:21 -0300

    fix: to_date function use to_datetime and checks if the str is a digit

--- src/etl_base/util/dataframe.py
+++ src/etl_base/util/dataframe.py
@@ -12,14 +12,12 @@ class PandasUtils:
             errors=kwargs.get('errors', 'coerce')
         )
 
-    @staticmethod
-    def to_date(col, **kwargs):
-        return pd.to_datetime(
-            col,
-            format=kwargs.get('format', '%Y-%m-%d'),
-            errors=kwargs.get('errors', 'coerce')
-        ).dt.date
+    @classmethod
+    def to_date(cls, col, **kwargs):
+        return cls.to_datetime(col, **kwargs).dt.date
 
     @staticmethod
     def to_interger(col):
-        return col.apply(lambda x : int(x) if pd.notnull(x) else np.NaN)
+        return col.apply(
+            lambda x : int(x) if pd.notnull(x) and str.isdigit(x) else np.NaN
+        )
